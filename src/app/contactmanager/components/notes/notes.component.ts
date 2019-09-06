f6bf3411a9fbfdcf300abbc5596ae08c06b4b6e8
Arun Ramachandriya <Arun.Ramachandriya@investec.co.uk>
2019-09-06 15:06:34 +0100

    Notes displayed in data table

--- src/app/contactmanager/components/notes/notes.component.ts
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, NgZone } from '@angular/core';
+
+import { MatTableDataSource } from '@angular/material';
 
 import { Note } from '../../models/note';
 
@@ -8,12 +10,24 @@ import { Note } from '../../models/note';
   styleUrls: ['./notes.component.scss']
 })
 export class NotesComponent implements OnInit {
-
-  @Input() notes: Note[];
   
-  constructor() { }
+  // @Input() notes: Note[];
 
-  ngOnInit() {
+  displayedColumns: string[] = ['#', 'title', 'date'];
+  dataSource: MatTableDataSource<Note>;
+  
+  constructor(private zone: NgZone) { }
+
+  @Input()
+  set notes(val: Note[]) {
+    this.dataSource = new MatTableDataSource<Note>(val);
+    console.log(`notes property set with: ${val}`);
   }
 
+  ngOnInit() {
+    console.log('ngOnInit() fired on NotesComponent')
+    // // this.zone.run(() => this.dataSource = this.notes);
+    // this.dataSource = new MatTableDataSource<Note>(this.notes);
+    // console.log(`this.datasource: ${this.dataSource}`);
+  }
 }
