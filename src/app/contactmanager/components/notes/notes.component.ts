ae68bd1724fd1660970a32fda8ffef1ab8342bdd
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-09-06 16:31:52 +0100

    Table pagination works

--- src/app/contactmanager/components/notes/notes.component.ts
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, OnInit } from '@angular/core';
+
+import { MatTableDataSource, MatPaginator } from '@angular/material';
 
 import { Note } from '../../models/note';
 
@@ -7,18 +9,22 @@ import { Note } from '../../models/note';
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.scss']
 })
-export class NotesComponent {
+export class NotesComponent implements OnInit {
   
-  // @Input() notes: Note[];
+  @ViewChild(MatPaginator, {static: true}) matPaginator: MatPaginator;
 
   displayedColumns: string[] = ['#', 'title', 'date'];
-  dataSource: Note[] = [];
+  dataSource: MatTableDataSource<Note>;
   
   constructor() { }
 
+  ngOnInit() {
+    this.dataSource.paginator = this.matPaginator;
+  }
+
   @Input()
   set notes(val: Note[]) {
-    this.dataSource = val;
+    this.dataSource = new MatTableDataSource<Note>(val);
     console.log(`notes property set with: ${val}`);
   }
 }
