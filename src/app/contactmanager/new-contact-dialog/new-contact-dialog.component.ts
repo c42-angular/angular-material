291f4407650bf34fd7cf0ea3d1e9675681451d71
Arun Ramachandriya <Arun.Ramachandriya@investec.co.uk>
2019-09-10 07:51:47 +0100

    New user add dialog - form elements added

--- src/app/contactmanager/new-contact-dialog/new-contact-dialog.component.ts
+++ src/app/contactmanager/new-contact-dialog/new-contact-dialog.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material';
+
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-new-contact-dialog',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewContactDialogComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  avatars = ['svg-1', 'svg-2', 'svg-3', 'svg-4']
+
+  constructor(private dialogRef: MatDialogRef<NewContactDialogComponent>) { }
 
   ngOnInit() {
+    this.user = new User();
   }
 
+  save() {
+this.dialogRef.close(this.user);
+  }
+
+  dismiss() {
+    this.dialogRef.close(null);
+  }
 }
